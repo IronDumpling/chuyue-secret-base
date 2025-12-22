92ca4a39e0e4fe53e91a75d9ed6e088311661234
IronDumpling <zhangchuyue.zhang@mail.utoronto.ca>
2025-12-21 23:15:54 -0500

    fix deploy pipeline

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
